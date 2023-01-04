6823195ef19a23e432f1ac26f4ce5ae1f96de6e8
Roman-code-web <selenerg.275@gmail.com>
2023-01-04 14:08:13 -0500

    Se agrego validacion funciona

--- src/app/componentes/form/form.component.ts
+++ src/app/componentes/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -6,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit  {
+  //declaramos variables 
+  public formRegistro!:FormGroup;
+  //constructor
+  constructor(private formRegistrobuilder:FormBuilder){
+
+  }
+
   ngOnInit(): void {
-    
+    this.formRegistro=this.formRegistrobuilder.group({
+      nombre:['',
+        [
+          Validators.required,
+          Validators.minLength(3)
+        ]
+      ],
+      correo:['',
+        [
+          Validators.required,
+          Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')
+        ]
+      ],
+      mensaje:['',
+        [
+          Validators.required,
+          Validators.minLength(3)
+        ]
+      ],
+    });
   }
   enviar(){
-
+    console.log(this.formRegistro.value)
   }
 }
